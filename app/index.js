22ecf02281e52ef74f5d02acfd0802f2cec62cf1
Kerem AR <keremar0000@gmail.com>
2024-10-16 21:30:02 +0300

    repo created

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text, ActivityIndicator } from 'react-native'
+import React from 'react'
+
+
+export default function StartPage() {
+  return (
+    <View  className="flex-1 justify-center items-center">
+      <Text className="text-2xl">StartPage</Text>
+      {/* <ActivityIndicator size="large" color="gray" /> */}
+    </View>
+  )
+}
---
+++ app/signIn.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+
+export default function signIn() {
+  return (
+    <View className="pt-20">
+      <Text>signin</Text>
+    </View>
+  )
+}
